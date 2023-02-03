25844fec22c4ee2823285ce5cef4e9972814ae67
angelsoto21 <angelsotto21@gmail.com>
2023-02-02 22:50:50 -0700

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://angelsoto21.github.io',
+  base: '/Astro-Proyect',
   integrations: [tailwind()]
 });
